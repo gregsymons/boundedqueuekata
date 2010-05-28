24aba7499fec6395d587888f2df09074850a01c0
Greg Symons <greg@Symons-Linux1.(none)>
2010-05-28 14:32:32 -0500

    Step nine, tests pass

--- src/BoundedQueue.h
+++ src/BoundedQueue.h
@@ -7,7 +7,7 @@
 template <typename T>
 class BoundedQueue {
 public:
-    BoundedQueue(QueueControl& producer, QueueControl& consumer) : consumer(consumer)
+    BoundedQueue(QueueControl& producer, QueueControl& consumer, size_t bound = 100) : consumer(consumer), producer(producer), bound(bound)
     { 
         producer.Resume(); 
         consumer.Pause();
@@ -20,21 +20,30 @@ public:
         {
             consumer.Resume();
         }
+        
+        if (q.size() > bound) 
+        {
+            producer.Pause();
+        }
     };
 
     const T& dequeue() 
     { 
        const T& item = q.front();
        q.pop();
+       
        if (q.size() == 0) 
        {
            consumer.Pause();
        }
+
        return item;
     } 
 
 private:
     QueueControl& consumer;
+    QueueControl& producer;
+    size_t bound;
     std::queue<T> q;
 };
 
